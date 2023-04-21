d99371ee71022e2381ae138d93dff41473733156
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-04-21 17:03:12 -0500

    Got Article from Article database to appear on the Dom inside the Modal

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,15 +4,27 @@ import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import { useSelector, useDispatch } from "react-redux";
 import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Portfolio = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const articleDetails = useSelector((store) => store.articleDetails);
-  const user = useSelector((store) => store.user);
   // console.log("inside portfolio", user);
   const { id } = useParams();
-  //console.log("article details", articleDetails);
+  console.log("article details", articleDetails);
   const [open, setOpen] = useState(false);
   const handleOpen = () => {
     setOpen(true);
@@ -31,52 +43,46 @@ const Portfolio = () => {
   useEffect(() => {
     getArticleDetails();
   }, []);
-
+  console.log(articleDetails);
   return (
     <div className="grid">
       {articleDetails.length > 0 ? (
         articleDetails.map((articleDetails) => (
           <div key={articleDetails.id}>
-            <div>{articleDetails.content}</div>
-            <img src={articleDetails.image_url} alt={articleDetails.title} />
+            <Button
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onClick={handleOpen}
+            >
+              What it’s like to specialize in psychiatry: Shadowing Dr. Hart
+            </Button>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  What it’s like to specialize in psychiatry: Shadowing Dr. Hart
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  {articleDetails.content}
+                </Typography>
+                <a href="https://www.startribune.com/inspired-s-race-and-equity-series-explores-mental-health-disparities-with-psychiatrist-dr-dionne-ha/600046918/?refresh=true">
+                  Link to Full Article
+                </a>
+              </Box>
+            </Modal>
+            <Button>Leave A Comment</Button>
           </div>
         ))
       ) : (
         <div>Loading...</div>
       )}
-      <Button
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        onClick={handleOpen}
-      >
-        What it’s like to specialize in psychiatry: Shadowing Dr. Hart
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="parent-modal-title"
-        aria-describedby="parent-modal-description"
-      >
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: 400,
-          }}
-        >
-          <h2 id="parent-modal-title">Text in a modal</h2>
-          <p id="parent-modal-description">
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </p>
-
-          {/* <ChildModal /> */}
-        </Box>
-      </Modal>
-      <Button>Leave A Comment</Button>
     </div>
   );
 };
