cc3f7c94659960126b01fc1baab0ca998eb8ab91
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-04-24 19:24:33 -0500

    comment form in middle of page and buttons lined up

--- src/components/RequestToBook/RequestToBook.jsx
+++ src/components/RequestToBook/RequestToBook.jsx
@@ -12,85 +12,88 @@ const RequestToBook = () => {
     Email: "",
   });
   return (
-    <div sx={{ display: "flex", justifyContent: "center" }}>
-      <Box
-        component="form"
-        sx={{
-          display: "inline-grid",
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-          maxwidth: "500px",
-          margin: "auto",
-        }}
-        noValidate
-        autoComplete="off"
-        justifyContent={"center"}
-      >
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Date"
-          multiline
-          required
-          maxRows={4}
-        />
-        <TextField
-          id="outlined-textarea"
-          label="Name"
-          placeholder="Name"
-          required
-          multiline
-        />
-
-        <TextField
-          id="outlined-textarea"
-          label="Email"
-          placeholder="Email"
-          required
-          multiline
-        />
-        <TextField
-          id="outlined-multiline-static"
-          label="Request"
-          multiline
-          required
-          rows={10}
-          defaultValue="Request"
-        />
-
-        <Button
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div>
+        <Box
+          component="form"
           sx={{
-            backgroundColor: "#FF91a4",
-            color: "green",
-            fontWeight: "bold",
-            border: "2px solid green",
-            borderRadius: "5px",
-            padding: "12px 12px",
-            cursor: "pointer",
-            width: "30%",
-            boxshadow: 3,
-            marginRight: "10px", // add margin on the right to have space between buttons
+            display: "inline-grid",
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+            maxwidth: "500px",
+            margin: "auto",
           }}
+          noValidate
+          autoComplete="off"
+          justifyContent={"center"}
         >
-          Edit
-        </Button>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Date"
+            multiline
+            required
+            maxRows={4}
+          />
+          <TextField
+            id="outlined-textarea"
+            label="Name"
+            placeholder="Name"
+            required
+            multiline
+          />
 
-        <Button
-          sx={{
-            backgroundColor: "#FF91a4",
-            color: "green",
-            fontWeight: "bold",
-            border: "2px solid green",
-            borderRadius: "5px",
-            padding: "12px 12px",
-            cursor: "pointer",
-            width: "30%",
-            boxShadow: 3,
-            marginLeft: "10px", //add a space on the left between buttons
-          }}
-        >
-          {" "}
-          Submit
-        </Button>
-      </Box>
+          <TextField
+            id="outlined-textarea"
+            label="Email"
+            placeholder="Email"
+            required
+            multiline
+          />
+          <TextField
+            id="outlined-multiline-static"
+            label="Request"
+            multiline
+            required
+            rows={10}
+            defaultValue="Request"
+          />
+          <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+            <Button
+              sx={{
+                backgroundColor: "#FF91a4",
+                color: "green",
+                fontWeight: "bold",
+                border: "2px solid green",
+                borderRadius: "5px",
+                padding: "12px 12px",
+                cursor: "pointer",
+                width: "40%",
+                boxshadow: 3,
+                marginRight: "10px", // add margin on the right to have space between buttons
+              }}
+            >
+              Edit
+            </Button>
+
+            <Button
+              sx={{
+                backgroundColor: "#FF91a4",
+                color: "green",
+                fontWeight: "bold",
+                border: "2px solid green",
+                borderRadius: "5px",
+                padding: "12px 12px",
+                cursor: "pointer",
+                width: "40%",
+                boxShadow: 3,
+                marginLeft: "10px", //add a space on the left between buttons
+              }}
+            >
+              {" "}
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </div>
     </div>
   );
 };
